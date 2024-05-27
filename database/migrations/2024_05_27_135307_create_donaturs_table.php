e82e51853377449f61ae428b82eaef32a9c7dcc0
Bob0I0 <159877192+Bob0I0@users.noreply.github.com>
2024-05-27 21:05:33 +0700

    Menyesuaikan kebutuhan tabel
    menambah nama, jumlah donasi, dan keterangan

--- database/migrations/2024_05_27_135307_create_donaturs_table.php
+++ database/migrations/2024_05_27_135307_create_donaturs_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('donaturs', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('amount');
+            $table->text('description');
             $table->timestamps();
         });
     }
