652bbcfec0838b096ffec84ed2d700611b35d386
Bob0I0 <159877192+Bob0I0@users.noreply.github.com>
2024-05-27 22:22:53 +0700

    View Donatur
    Update kode view donatur

--- resources/views/donaturs/index.blade.php
+++ resources/views/donaturs/index.blade.php
@@ -1,3 +1,59 @@
-<div>
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+@extends('layouts.app')
+@section('content')
+<div class="card">
+    <div class="card-header">List Donatur</div>
+    <div class="card-body">
+        @can('create-donatur')
+        <a href="{{ route('donaturs.create') }}" class="btn btn-success btn-sm my-2"><i class="bi bi-plus-circle"></i> Add New Donatur</a>
+        @endcan
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">S#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($donaturs as $donatur)
+                <tr>
+                    <th scope="row">{{ $loop->iteration }}</th>
+                    <td>{{ $donatur->name }}</td>
+                    <td>{{ $donatur->amount }}</td>
+                    <td>{{ $donatur->description }}</td>
+                    <td>
+                        <form action="{{ route('donaturs.destroy', $donatur->id) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+
+                            <a href="{{ route('donaturs.show', $donatur->id) }}" class="btn btn-warning btn-sm"><i class="bi bi-eye"></i>
+                                Show</a>
+
+                            @can('edit-donatur')
+                            <a href="{{ route('donaturs.edit', $donatur->id) }}" class="btn btn-primary btn-sm"><i class="bi bi-pencil-square"></i> Edit</a>
+                            @endcan
+
+                            @can('delete-donatur')
+                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Do you want to delete this donatur?');"><i class="bi bi-trash"></i> Delete</button>
+
+                            @endcan
+                        </form>
+                    </td>
+                </tr>
+                @empty
+                <td colspan="4">
+                    <span class="text-danger">
+                        <strong>No Donatur Found!</strong>
+                    </span>
+                </td>
+                @endforelse
+            </tbody>
+        </table>
+
+        {{ $donaturs->links() }}
+
+    </div>
 </div>
+@endsection
--- resources/views/donaturs/show.blade.php
+++ resources/views/donaturs/show.blade.php
@@ -1,3 +1,46 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card-header">
+            <div class="float-start">
+                Donatur Information
+            </div>
+            <div class="float-end">
+                <a href="{{ route('donaturs.index') }}" class="btn btn-primary btn-sm">&larr; Back</a>
+
+            </div>
+        </div>
+        <div class="card-body">
+
+            <div class="row">
+                <label for="name" class="col-md-4 col-form-label text-md-end text-start"><strong>Name:</strong></label>
+
+                <div class="col-md-6" style="line-height: 35px;">
+                    {{ $donatur->name }}
+                </div>
+            </div>
+
+            <div class="row">
+                <label for="amount" class="col-md-4 col-form-label text-md-end text-start"><strong>Amount:</strong></label>
+
+                <div class="col-md-6" style="line-height: 35px;">
+                    {{ $donatur->amount }}
+                </div>
+            </div>
+
+            <div class="row">
+                <label for="description" class="col-md-4 col-form-label text-md-end text-start"><strong>Description:</strong></label>
+                <div class="col-md-6" style="line-height: 35px;">
+                    {{ $donatur->description }}
+                </div>
+            </div>
+
+        </div>
+    </div>
 </div>
+</div>
+
+@endsection
