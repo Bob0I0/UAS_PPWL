76e3d169c612c8153c73f738ef943ec03f24b516
Bob0I0 <159877192+Bob0I0@users.noreply.github.com>
2024-05-27 22:29:57 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,30 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     {{ __('You are logged in!') }}
+                    <p>This is your application dashboard.</p>
+
+                    @canany(['create-role', 'edit-role', 'delete-role'])
+                    <a class="btn btn-primary" href="{{ route('roles.index') }}"><i class="bi bi-person-fill-gear"></i> Manage Roles</a>
+                    @endcanany
+
+                    @canany(['create-donatur', 'edit-donatur','delete-donatur'])
+                    <a class="btn btn-warning" href="{{ route('donaturs.index') }}"><i class="bi bi-bag"></i> Manage Donaturs</a>
+                    @endcanany
+
+                    <p>&nbsp;</p>
+                    
                 </div>
             </div>
         </div>
