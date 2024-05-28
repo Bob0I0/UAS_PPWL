a7f6f1688efb2698bb7d33d43aca420afb5dc361
Bob0I0 <159877192+Bob0I0@users.noreply.github.com>
2024-05-28 19:41:58 +0700

    Update StoreDonaturRequest.php
    ketinggalan perbaikan

--- app/Http/Requests/StoreDonaturRequest.php
+++ app/Http/Requests/StoreDonaturRequest.php
@@ -23,7 +23,7 @@ class StoreDonaturRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:250',
-            'amount' => 'rquired|integer',
+            'amount' => 'required|integer',
             'description' => 'required|string'
         ];
     }
